16432282654f723500cfcd6fa4be7dfce6d63f93
cicada3318 <92419549+cicada3318@users.noreply.github.com>
2025-10-30 23:03:39 +0500

    Day two

---
+++ Day2.java
@@ -0,0 +1,43 @@
+public class Day2 {
+    public static void main(String[] args) {
+        
+        // var_type var_name;
+        int Java4Noob$;
+
+        // NAMING RULES:
+        // In variable names, you can use alpha-numeric words.
+        // Varibles must not start with a number.
+        // No special symbols used except $ and _
+
+        // 8 Datatypes in Java
+        int number = 45;
+        long long_number = 45000000000l; // l
+        short small_number = 200;
+        byte smaller_number = 120; // -128 to 127
+
+        float point_numbers = 4.5f; // f
+        double long_point_number = 4.102814852214;
+
+        boolean true_or_false = true;
+        char character = 'f';
+        System.out.println(character);
+        character++;
+        System.out.println(character);
+
+// OPERATORS
+// no need, already covered in C 
+
+// Strings
+
+String string_name = "Java for noooooooooooobs";
+// String is not a datatype but treated as one.
+
+String str = new String("One more way!");
+
+System.out.println(string_name);
+    }
+}
+
+
+// Research on some string methods like .length, .uppercase etc.
+// Do some operation on the datatypes above.
