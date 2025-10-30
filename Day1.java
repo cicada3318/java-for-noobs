0c47a7b3bad5729975cd96858d2db73807f24122
cicada3318 <92419549+cicada3318@users.noreply.github.com>
2025-10-30 19:27:30 +0500

    Day one.
    Install and setup Java, and hello world!

---
+++ Day1.java
@@ -0,0 +1,18 @@
+/*
+ * - Download and install JDK on your PC.
+ * (https://download.oracle.com/java/25/latest/jdk-25_windows-x64_bin.exe)
+ * - Download VS Code. (https://code.visualstudio.com/download)
+ * - Download two extensions on vs code, "Java" and "Code runner".
+ * - Reboot your PC.
+ * - Create a file i.e Day1.java and run it with he following code.
+ * 
+ */
+
+public class Day1 { // ClassName = Filename
+    public static void main(String[] args) {
+        System.out.println("Java for Noobs");
+    }
+}
+
+// Today's activity: 
+// Create a file named after your name and write a basic "Welcome program!"
